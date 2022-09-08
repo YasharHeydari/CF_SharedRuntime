b6a5e256072cf8b150522e677d14697b5685c4ba
Yashar.Heydari <yashar.heydari@canalyst.com>
2022-09-08 14:15:31 -0700

    Shared Runtime demo

---
+++ src/functions/functions.ts
@@ -0,0 +1,16 @@
+﻿/* global console, CustomFunctions */
+
+/**
+ * Adds two numbers.
+ * @customfunction
+ * @param first First number
+ * @param second Second number
+ * @param {CustomFunctions.Invocation} invocation Invocation Object
+ * @returns The sum of the two numbers.
+ * @requiresAddress
+ */
+export function add(first: number, second: number, invocation: CustomFunctions.Invocation): number {
+  console.log(invocation);
+  return first + second;
+}
+
